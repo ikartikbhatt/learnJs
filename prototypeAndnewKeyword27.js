3255eb771674638ddcfb38e370e383528be273d9
kartik <149373290+ikartikbhatt@users.noreply.github.com>
2025-04-30 20:30:34 +0530

    comitted

---
+++ prototypeAndnewKeyword27.js
@@ -0,0 +1,47 @@
+//prototype
+//new keyword
+
+// function hello() {
+//     console.log("hello");
+    
+// }
+
+
+// hello();
+
+//javascript function ==> function + object 
+
+//we can add our own properties
+
+//hello.myOwnProperty = "very unique"
+//console.log(hello.myOwnProperty);
+
+
+//function provides us free space i.e. empty object{}
+
+// that empty object / free space is called prototype
+//console.log(hello.prototype);  // {}
+
+
+//only fn provides prototype properties
+
+//check to validate 
+
+// function hello() {
+//     console.log("hello");
+    
+// } // here it will give the prototype ==> true
+
+//const hello={key:'value1'} // here it will not  give the prototype ==> false
+//const hello=['value1'] // here it will not  give the prototype ==> false
+
+if (hello.prototype) {
+    console.log('prototype is present');
+    
+}
+else{
+    console.log('prototype not present');
+    
+}
+
+
